3d2bf6bcf4adeeb1f40b156514f72fc797977cd0
MOSMekawy <49022449+MOSMekawy@users.noreply.github.com>
2021-07-18 01:23:47 +0200

    minor refactoring to schema class

--- src/schema.js
+++ src/schema.js
@@ -19,45 +19,57 @@ class Schema {
         try {
           dest = dest[node];
         } catch {
-          throw new Error("SCHEMA_OBJECT_MISMATCH");
+          throw new SchemaObjectMismatchError("SCHEMA_OBJECT_MISMATCH");
         }
       });
 
       Object.keys(field.validations).forEach((key) => {
         let _eval;
 
-        if (key == "_isLeaf") return;
+        if (key === "_isLeaf") return;
 
-        if (typeof field.validations[key] == "function") {
+        if (typeof field.validations[key] === "function") {
           _eval = field.validations[key](dest);
-          if (!_eval)
-            err_stack.push(
-              `${key} validator deems the field ${field_name} invalid.`
-            );
-        }
-        if (typeof field.validations[key] == "object") {
+          if (_eval) return;
+          err_stack.push(`${key} validator deems the field ${field_name} invalid.`);
+        } else if (typeof field.validations[key] === "object") {
           _eval = field.validations[key].validator(dest);
-          if (!_eval) {
-            let err_msg = field.validations[key].message;
-            if (typeof err_msg == "undefined")
-              err_stack.push(
-                `${key} validator deems the field ${field_name} invalid.`
-              );
-            if (typeof err_msg != "function") err_stack.push(err_msg);
-            else if (typeof err_msg == "function")
-              err_stack.push(err_msg(field_name, key, dest));
-          }
+          if (_eval) return;
+          let err_msg = field.validations[key].message;
+          if (typeof err_msg === "undefined") err_stack.push(`${key} validator deems the field ${field_name} invalid.`);
+          else if (typeof err_msg !== "function") err_stack.push(err_msg);
+          else if (typeof err_msg === "function") err_stack.push(err_msg(field_name, key, dest));
         }
       });
     });
 
-    if (err_stack.length != 0) {
-      const err = new Error("VALIDATION_ERROR");
-      err.errors = err_stack;
+    if (err_stack.length === 0) return;
+    
+    const err = new ValidationError("VALIDATION_ERROR");
+    err.errors = err_stack;
 
-      throw err;
-    }
+    throw err;
   }
+}
+
+class SchemaObjectMismatchError extends Error {
+
+  constructor(message) {
+    super(message);
+    this.name = "SchemaObjectMismatchError";
+  }  
+
+}
+
+class ValidationError extends Error {
+  
+  errors = [];
+
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
+
 }
 
 module.exports = Schema;
