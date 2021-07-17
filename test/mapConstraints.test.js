402c927a5b2b0131166240eff039e29a2868889b
MOSMekawy <49022449+MOSMekawy@users.noreply.github.com>
2021-07-17 05:31:45 +0200

    creating simple tests

---
+++ test/mapConstraints.test.js
@@ -0,0 +1,25 @@
+const { expect } = require("@jest/globals");
+const mapConstraints = require("../src/map-constraints");
+
+test("object path mapping", () => {
+  let truePaths = new Set([ ["email"], ["credential", "password"], ["credential", "token", "access_token"] ]);
+  let paths = mapConstraints({
+    email: {
+      _isLeaf: true,
+    },
+    credential: {
+      password: {
+        _isLeaf: true,
+      },
+      token: {
+        access_token: {
+          _isLeaf: true,
+        },
+      },
+    },
+  });
+  paths = paths.map(val => val.path);
+  paths = new Set(paths);
+
+  expect(paths).toEqual(truePaths);
+});
